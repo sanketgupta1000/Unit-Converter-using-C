b52e7d48cf10f1be86d3b003448508f5542e1444
Mokariya Pradip <122159962+MokariyaPradip@users.noreply.github.com>
2023-02-19 10:31:40 +0530

    added userfrendliness and fixed some bugs

--- main and sub functiions.c
+++ main and sub functiions.c
@@ -3,32 +3,47 @@ int no_of_qty;
 FILE *database;
 void main(void)
 {
-    //Taking input of number of physical quantities from the database file
-
-    //Skipping lines containing schemes from the database file and scanning
     database=fopen("Physical_Quantities.db", "r");
+
+    //Taking input of number of physical quantities from the database file
+    //Skipping lines containing schemes from the database file and scanning
     fscanf(database, "%*[^/]%*[^:]:%d", &no_of_qty);
     quantity qty[no_of_qty];
 
+    //to load all data from database to array of structure
     load_data(qty);
 
     //declaring variables for input and calculation
     int physical_quantity, initial_choice, final_choice;
     double initial_value, final_value;
-    char continue_flag;
-    
+    char continue_flag='Y';
+
+    print_line();
+    printf("\n\t\t\t\t\t*****Welcome to UNIT CONVERTER*****\n\n");
+
     do
     {
         do
         {
             physical_quantity = show_menu(qty);
+            print_line();
             initial_choice = show_sub_menu(qty, physical_quantity-1);
-        } while (!initial_choice);
+            if(!(initial_choice))
+            {
+                continue;
+            }
+           
+            printf("Enter the choice number to which you want to convert : ");
+            do 
+            {
+                scanf("%d", &final_choice);
+            }
+            while(((final_choice<0) || (final_choice>(qty[physical_quantity-1].no_of_units)) && (printf("Invalid choice please enter valid choice : "))));
+            print_line();
+        } while ((!(initial_choice)) || (!(final_choice)));
 
-        printf("Enter the choice number to which you want to convert : ");
-        scanf("%d", &final_choice);
-        printf("\nEnter the value : ");
-        scanf("%lf\n", &initial_value);
+        printf("Enter the value : ");
+        scanf("%lf", &initial_value);
 
 
         double base_converter_multiple = qty[physical_quantity - 1].qty_units[initial_choice - 1].unit_to_base_multiple;
@@ -45,10 +60,19 @@ void main(void)
         strcpy(initial_unit_name, qty[physical_quantity - 1].qty_units[initial_choice - 1].unit_name);
         strcpy(final_unit_name, qty[physical_quantity - 1].qty_units[final_choice - 1].unit_name);
 
+        printf("Here is your conversion...\n");
         printf("%lf %s = %lf %s\n", initial_value, initial_unit_name, final_value, final_unit_name);
 
-        printf("Do you wish to convert another physical quantity?\nEnter 'Y' to continue, otherwise enter 'N' : ");
-        scanf("%c", &continue_flag);
+        print_line();
 
+        printf("Do you wish to convert another physical quantity?\nEnter 'Y' to continue, otherwise enter 'N' : ");
+        scanf("\n%c", &continue_flag);
+
+        
     } while ((continue_flag == 'Y') || (continue_flag == 'y'));
+    
+    print_line();
+    printf("\n\t\t\t\t\t*****Thanks for choosing UNIT CONVERTER*****\n\n");
+    print_line();
+    fclose(database);
 }
