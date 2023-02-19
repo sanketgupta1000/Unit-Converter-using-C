5ded7ab4801bf6fb878163027f4116d5129f822c
Mokariya Pradip <122159962+MokariyaPradip@users.noreply.github.com>
2023-02-19 15:20:55 +0530

    included necessary header files and declared start_log function

--- helpers.h
+++ helpers.h
@@ -2,7 +2,9 @@
 as well as the structure definitions and declarations for the Unit Converter*/
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 #define MAX_UNITS 15
 #define NAME_LEN 30
@@ -30,3 +32,4 @@ int show_menu(quantity *);
 int show_sub_menu(quantity *, int);
 void print_line(void);
 void load_data(quantity *);
+FILE *start_log(void);
