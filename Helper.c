da3c1a66ed60598ea3cd5cc622428ec37ed8a455
Mokariya Pradip <122159962+MokariyaPradip@users.noreply.github.com>
2023-02-19 10:29:35 +0530

    Added user frendliness and fixed some bugs

--- Helper.c
+++ Helper.c
@@ -6,18 +6,26 @@ extern int no_of_qty;
 extern FILE * database;
 int show_menu(quantity *qty)
 {
+    print_line();
     int physical_quantity, i;
-    printf("Here is a list of Physical Quantities :\n");
+    printf("Here is the list of Physical Quantities...\n");
     for (i = 0; i < no_of_qty; i++)
     {
-        printf("\t%s : %d", qty[i].qty_name, i + 1);
+        printf("\t%-*s : %d", NAME_LEN-10 , qty[i].qty_name, i + 1);
         if (i % 2)
         {
             printf("\n");
         }
     }
     printf("\n");
-    scanf("%d", &physical_quantity);
+    printf("Enter chioce number of Physical Quantity you want to convert : ");
+    
+    //for invalid number
+    do
+    {
+        scanf("%d", &physical_quantity);
+    }
+    while(((physical_quantity<1)||(physical_quantity>no_of_qty)) && printf("Invalid choice please enter valid choice : "));
     return physical_quantity;
 }
 
@@ -25,22 +33,30 @@ int show_sub_menu(quantity *qty , int choice)
 {
     int i,initial_choice;
 
+    printf("\n\t\t\t\t\t****Welcome to %s CONVERSION****\n\n",qty[choice].qty_name);
+    print_line();
+
     printf("\tBack : 0\n");    
     //******for print the sub menu containing list of units*******
-    for(i=0;i<(((qty+choice)->no_of_units)-1);i+=2)
+    for(i=0;i<((qty[choice].no_of_units)-1);i+=2)
     {
-        printf("\t\t%s : %d\t%s :  %d\n", (qty[choice].qty_units[i].unit_name), i+1, (qty[choice].qty_units[i+1].unit_name), i+2);
+        printf("\t\t%-*s : %d\t\t%-*s :  %d\n", NAME_LEN, (qty[choice].qty_units[i].unit_name), i+1, NAME_LEN, (qty[choice].qty_units[i+1].unit_name), i+2);
     }
-    if(((qty+choice)->no_of_units) % 2 )
+    if((qty[choice].no_of_units) % 2 )
     {
-        printf("\t\t%s : %d\n",(qty[choice].qty_units[i].unit_name), i+1);
+        printf("\t\t%-*s : %d\n",NAME_LEN, (qty[choice].qty_units[i].unit_name), i+1);
     }
         
     printf("Enter the choice number from which you want to convert : ");
 
-    //to scan initial choice
-    scanf("%d\n",&initial_choice);
-    
+    //to scan initial choice and checking invalid number
+    do 
+    {
+        scanf("%d",&initial_choice);
+    }
+    while(((initial_choice<0) || (initial_choice>(qty[choice].no_of_units))) && (printf("Invalid choice please enter valid choice : ")));
+
+
     //returning the initial choice
     return initial_choice;
 }
@@ -49,11 +65,11 @@ int show_sub_menu(quantity *qty , int choice)
 //to print horizontal line
 void print_line(void)
 {
-    //for center alignment
-    printf("\t");
+    // for center alignment
+    // printf("\t");
     
     //for printing line 
-    for(int i=0; i < 80 ; i++)
+    for(int i=0; i < 115 ; i++)
     {
         printf("-");
     }
@@ -67,12 +83,12 @@ void load_data(quantity *qty)
 	for(i=0; i < no_of_qty ; i++)
 	{
 		//for scanning physical quantity's name and number of units of that quantity
-		fscanf(database, "%*[^|]| %[^|]%d", qty[i].qty_name, &qty[i].no_of_units);
+		fscanf(database, "%*[^|]| %[^|]| %d %*[^\n] ", qty[i].qty_name, &qty[i].no_of_units);
 		
 		for(j=0; j < qty[i].no_of_units ; j++)
 		{
 			//for scanning unit's name and multiple and constants related to that unit 
-			fscanf(database, "%*[^|]| %[^|]%lf%lf",qty[i].qty_units[j].unit_name, &qty[i].qty_units[j].unit_to_base_multiple, &qty[i].qty_units[j].unit_to_base_constant);
+			fscanf(database, "%*[^|]| %[^|]| %lf | %lf%*[^\n]",qty[i].qty_units[j].unit_name, &qty[i].qty_units[j].unit_to_base_multiple, &qty[i].qty_units[j].unit_to_base_constant);
 		}
 	}
 }
